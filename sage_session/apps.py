d2326bd47c207de58c7534962cd7decb88714953
abolfazl jazak <jazakabolfazl@gmail.com>
2024-08-25 17:41:51 +0330

    feat(apps): add verbose_name to Sage Session Management app
    Added the `verbose_name` attribute to the `Sage Session Management` app in `sage_session/apps.py` to provide a human-readable name for the application in Django admin and other interfaces.

--- sage_session/apps.py
+++ sage_session/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SageSessionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "sage_session"
+    verbose_name = "Sage Session Management"
