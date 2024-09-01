c4b5d6c88b9c3444644280a68ebe5531d2261c50
abolfazl jazak <jazakabolfazl@gmail.com>
2024-09-01 19:04:25 +0330

    test(sage_session): add unit tests for SessionHandler
    - Added unit tests in `sage_session/tests/handlers/test_session.py` to cover the following scenarios:
      - Storing and retrieving a session value.
      - Storing a session value with an expiry.
      - Deleting a session value.
      - Checking the existence of a session value.
      - Storing and retrieving a session value without encryption.
    - Included necessary fixtures and setup in `sage_session/tests/__init__.py` and `sage_session/tests/handlers/__init__.py`.

---
+++ sage_session/tests/handlers/test_session.py
@@ -0,0 +1,67 @@
+import time
+from datetime import timedelta
+
+import pytest
+from django.http import HttpRequest
+from sage_session.handlers.session import SessionHandler
+from django.conf import settings
+
+
+@pytest.fixture(autouse=True)
+def set_up_django_settings():
+    settings.SECRET_KEY = "secret"
+    settings.FERNET_SECRET_KEY = "o34uN6k2bR5Vz8XWcN7uFgfpX_Q0G63BRw8RXBbHrZI=!"
+    settings.SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+    settings.INSTALLED_APPS = ["django.contrib.sessions"]
+
+
+@pytest.fixture
+def http_request():
+    request = HttpRequest()
+    request.session = {}
+    return request
+
+
+def test_storge_and_retrieve_value(http_request):
+
+    session_handler = SessionHandler(http_request)
+    session_handler.set("username", "user_test")
+    assert session_handler.get("username") == "user_test"
+
+
+def test_storge_value_with_expiry(http_request):
+
+    session_handler = SessionHandler(http_request)
+    session_handler.set("username", "user_test", lifespan=timedelta(seconds=2))
+    assert session_handler.get("username") == "user_test"
+
+    time.sleep(2)
+
+    assert session_handler.get("username") is None
+
+
+def test_delete_value(http_request):
+
+    session_handler = SessionHandler(http_request)
+    session_handler.set("username", "user_test")
+
+    session_handler.delete("username")
+
+    assert session_handler.get("username") is None
+
+
+def test_check_value_existence(http_request):
+    session_handler = SessionHandler(http_request)
+
+    assert not session_handler.exists("username")
+
+    session_handler.set("username", "user_test")
+
+    assert session_handler.exists("username")
+
+
+def test_store_and_retrieve_value_without_encryption(http_request):
+
+    session_handler = SessionHandler(http_request)
+    session_handler.set("username", "user_test", encrypt=False)
+    assert session_handler.get("username", decrypt=False) == "user_test"
